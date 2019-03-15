ac672e134bde8d33c68d942dc2593cd18f75eea5
DmK78 <Ek193burg>
2019-03-15 11:05:55 +0500

    Initial commit

---
+++ app/src/main/java/com/android/dmk78/clubolympus/Data/ClubOlympusContract.java
@@ -0,0 +1,35 @@
+package com.android.dmk78.clubolympus.Data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public final class ClubOlympusContract {
+
+    public final static String DATABASE_NAME = "olumpusDB";
+    public final static int DATABASE_VERSION = 1;
+    public static final String SCHEME = "content://";
+    public static final String AUTHORITY = "com.android.dmk78.clubolympus";
+    public static final String PATH_MEMBERS = "members";
+    public static final Uri BASE_CONTENT_URI = Uri.parse(SCHEME + AUTHORITY);
+
+    private ClubOlympusContract() {
+    }
+
+    public static final class MemberEntry implements BaseColumns {
+
+
+        public static final String TABLE_NAME = "members";
+
+        public static final String COLUMN_ID = BaseColumns._ID;
+        public static final String COLUMN_FIRST_NAME = "firstName";
+        public static final String COLUMN_LAST_NAME = "lastName";
+        public static final String COLUMN_GENDER = "gender";
+        public static final String COLUMN_SPORT = "sport";
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_MEMBERS);
+    }
+}
---
+++ app/src/main/java/com/android/dmk78/clubolympus/MainActivity.java
@@ -0,0 +1,83 @@
+package com.android.dmk78.clubolympus;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.dmk78.clubolympus.Data.ClubOlympusContract.MemberEntry;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView dataTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dataTextView = findViewById(R.id.dataTextView);
+
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, com.android.dmk78.clubolympus.AddMemberActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        displayData();
+    }
+
+    private void displayData() {
+        String projection[] = {
+                MemberEntry._ID,
+                MemberEntry.COLUMN_FIRST_NAME,
+                MemberEntry.COLUMN_LAST_NAME,
+                MemberEntry.COLUMN_GENDER,
+                MemberEntry.COLUMN_SPORT
+        };
+        Cursor cursor = getContentResolver().query(
+                MemberEntry.CONTENT_URI,
+                projection, null, null, null
+        );
+        dataTextView.setText("All members\n\n");
+        dataTextView.append(MemberEntry._ID + " " +
+                MemberEntry.COLUMN_FIRST_NAME + " " +
+                MemberEntry.COLUMN_LAST_NAME + " " +
+                MemberEntry.COLUMN_GENDER + " " +
+                MemberEntry.COLUMN_SPORT);
+
+        int idIndex = cursor.getColumnIndex(MemberEntry._ID);
+        int idFirstName = cursor.getColumnIndex(MemberEntry.COLUMN_FIRST_NAME);
+        int idLastName = cursor.getColumnIndex(MemberEntry.COLUMN_LAST_NAME);
+        int idGender = cursor.getColumnIndex(MemberEntry.COLUMN_GENDER);
+        int idSport = cursor.getColumnIndex(MemberEntry.COLUMN_SPORT);
+
+        while (cursor.moveToNext()){
+            int curentId = cursor.getInt(idIndex);
+            String curentFirstName = cursor.getString(idFirstName);
+            String curentLastName = cursor.getString(idLastName);
+            int curentGender = cursor.getInt(idGender);
+            String curentSport = cursor.getString(idSport);
+
+            dataTextView.append("\n+" +
+                    curentId+" "+
+                    curentFirstName+" "+
+                    curentLastName+" "+
+                    curentGender+" "+
+                    curentSport);
+        }
+        cursor.close();
+
+    }
+}
